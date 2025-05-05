937ae508cdbeae896c3bb2c393760940d35c61b3
Rafal Staszak <staszak.raf@gmail.com>
2025-05-05 20:55:28 +0000

    first commit

---
+++ turtlebot3_autocontrol/traverse.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data, HistoryPolicy
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Header
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import TwistStamped
+
+from .transformations import euler_from_quaternion
+import threading
+
+
+class Traverse(Node):
+    def __init__(self):
+        super().__init__('traverse')
+        self.laser_scan: LaserScan = None
+        self.odom: Odometry = None
+
+        self.new_scan = False
+        self.new_odom = False
+
+        self.laser_scan_sub = self.create_subscription(LaserScan, "/scan", self.scan_callback,
+                                                       qos_profile=qos_profile_sensor_data)
+        
+        self.odometry_sub = self.create_subscription(Odometry, "/odom", self.odom_callback,
+                                                       qos_profile=qos_profile_sensor_data)
+        
+        self.cmd_vel_pub = self.create_publisher(TwistStamped, "/cmd_vel", qos_profile=HistoryPolicy.KEEP_LAST)
+
+        self.run_forward()
+        
+    
+    def stop(self):
+        msg = TwistStamped()
+        msg.header = Header(stamp=self.get_clock().now())
+        msg.twist.angular.z = 0.0
+        msg.twist.linear.x = 0.0
+        
+        self.cmd_vel_pub.publish(msg)
+
+    def run_forward(self, vel=0.1):
+        msg = TwistStamped()
+        msg.header = Header(stamp=self.get_clock().now())
+        msg.twist.angular.z = 0.0
+        msg.twist.linear.x = vel
+        
+        self.cmd_vel_pub.publish(msg)
+
+    def turn_left(self, vel = 0.1, ang_vel = 0.5):
+        msg = TwistStamped()
+        msg.header = Header(stamp=self.get_clock().now())
+        msg.twist.angular.z = ang_vel
+        msg.twist.linear.x = vel
+        
+        self.cmd_vel_pub.publish(msg)
+
+    def turn_right(self, vel = 0.1, ang_vel = .5):
+        msg = TwistStamped()
+        msg.header = Header(stamp=self.get_clock().now())
+        msg.twist.angular.z = -ang_vel
+        msg.twist.linear.x = vel
+        
+        self.cmd_vel_pub.publish(msg)
+
+    def scan_callback(self, data):
+        self.laser_scan = data
+        self.new_scan = True
+
+    def odom_callback(self, data):
+        self.odom = data
+        self.new_odom = True
+
+
+    def process(self):
+        if self.new_scan and self.new_odom:
+            self.new_scan = False
+            self.new_odom = False
+
+            # x = self.odom.pose.pose.position.x
+            # y = self.odom.pose.pose.position.y
+            # q = self.odom.pose.pose.orientation
+            # euler = euler_from_quaternion([q.x, q.y, q.z, q.w])
+            # angle_min = self.laser_scan.angle_min
+            # angle_max = self.laser_scan.angle_max
+            # angle_inc = self.laser_scan.angle_increment
+            # ranges = self.laser_scan.ranges
+
+            # position = self.odom.pose.pose.position
+            # self.run_forward()
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = Traverse()
+    rate = node.create_rate(100)
+
+    thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
+    thread.start()
+
+    while rclpy.ok():
+        node.process()
+        rate.sleep()
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
